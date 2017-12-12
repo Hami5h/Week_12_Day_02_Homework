8800db2dd220745feef3b074b84ea2c5ac2af1b9
Hamish <hamishfpso@gmail.com>
2017-12-12 20:14:58 +0000

    Added stop bounce for markers and where am I button

--- public/app.js
+++ public/app.js
@@ -6,13 +6,19 @@ const initialize = function() {
   const container = document.querySelector('#main-map');
   const mainMap = new MapWrapper(container, center, 18);
   mainMap.addClickEvent();
-  mainMap.addMarker(center);
+  mainMap.addMarker(center, "This is CodeClan");
 
   const buttonBounce = document.querySelector('#bounce');
   buttonBounce.addEventListener('click', mainMap.bounceMarkers.bind(mainMap));
 
+  const buttonStopBounce = document.querySelector('#stop-bounce');
+  buttonStopBounce.addEventListener('click', mainMap.stopBounce.bind(mainMap));
+
   const buttonChicago = document.querySelector('#chicago');
   buttonChicago.addEventListener('click', mainMap.toChicago.bind(mainMap));
+
+  const buttonLocation = document.querySelector('#current-location');
+  buttonLocation.addEventListener('click', mainMap.whereAmI.bind(mainMap));
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
--- public/mapWarpper.js
+++ public/mapWarpper.js
@@ -14,13 +14,13 @@ MapWrapper.prototype.addClickEvent = function() {
       lat: event.latLng.lat(),
       lng: event.latLng.lng()
     };
-    this.addMarker(position);
+    this.addMarker(position, "You selected this location");
   }.bind(this));
 }
 
-MapWrapper.prototype.addMarker = function(coords) {
+MapWrapper.prototype.addMarker = function(coords, message) {
   const infoWindowLocation = new google.maps.InfoWindow({
-    content: "This is great",
+    content: message + ' ' + `${coords.lat}, ${coords.lng}`
   });
   const marker = new google.maps.Marker({
     position: coords,
@@ -28,7 +28,7 @@ MapWrapper.prototype.addMarker = function(coords) {
     infoWindow: infoWindowLocation
   });
   google.maps.event.addListener(marker, 'click', function() {
-    this.infoWindow.open(this.googleMap, marker);
+    this.infoWindow.open(this.googleMap, this);
   });
   this.markers.push(marker);
 }
@@ -39,12 +39,29 @@ MapWrapper.prototype.bounceMarkers = function() {
   });
 }
 
+MapWrapper.prototype.stopBounce = function() {
+  this.markers.forEach(function(marker){
+    marker.setAnimation(null);
+  })
+}
+
 MapWrapper.prototype.toChicago = function() {
   const chicago = {
     lat: 41.854073,
     lng: -87.619392,
   }
   this.googleMap.setCenter(chicago);
-  this.addMarker(chicago);
+  this.addMarker(chicago, "This is Chicago");
   this.googleMap.setZoom(16);
+}
+
+MapWrapper.prototype.whereAmI = function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    const location = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    }
+    this.googleMap.setCenter(location);
+    this.addMarker(location, 'This is your current location');
+  }.bind(this))
 }
